fa22c6f640942804b6c85c19fd93f681b8632532
Zac Crabtree <zac.crabtree1@gmail.com>
2022-11-17 19:15:33 -0600

    added takeoff and landing functionality

--- scripts.js
+++ scripts.js
@@ -1,2 +1,30 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+
+    //get data you need
+    //define the objects you need to access and the variables you will use
+const takeoffbutton = document.getElementById("takeoff");
+const landingbutton = document.getElementById("landing");
+    //put content on page to be displayed immediately
+    //listen for events and interact with user (declare any additional objects that don't need
+    //to be in scope for other listeners)
+    takeoffbutton.addEventListener("click", function() {
+        let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if(result) {
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = 10000;
+        }
+    });
+    
+    landingbutton.addEventListener("click", function() {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
+        document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = 0;
+    });
+    
+    //Add utility functions at the end
+
+})
